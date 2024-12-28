5b34de293561c4ffdd1a4ecaeb3d201abab71b97
Sukanth Kutuwan <98080536+sukkkant@users.noreply.github.com>
2024-12-28 17:41:00 +0530

    Add files via upload

---
+++ Practical1.py
@@ -0,0 +1,80 @@
+# 1] D=[1,2,3,4,5,6,7,8,9,10]
+
+# 2] Max_value=max(D)
+# print(Max_value)
+
+# 2] Min_value=min(D)
+# print(Min_value)
+
+# 3] D.insert(3,4)
+# print(D)
+#   
+# 4] D=[1,2,3,4,4,4,5,6,7,8,9,10]
+# D1=[2,3,4,5,6,7]
+# D.pop(3)
+# D.pop(3)
+# print(D)
+
+# 5] D.reverse()
+# print(D)
+
+# 6] print(D.count(4))
+
+# 7] D3=D+D1
+# print(D3)
+
+# 8] D.sort()
+# print(D) The second largest number in the list is 9.
+
+# 9] D.sort(reverse=True)
+# print(D)
+
+# 10] value=3
+# l_ist=[1,2,3,4,5]
+# if value in l_ist:
+#     print(str(value )+ "exists in list")
+# else:
+#     print(str(value) + "does not exist in list")
+
+# 11] D.append(11)
+# print(D)
+
+# D.insert(12,12)
+# print(D)
+
+# D.extend(D1)
+# print(D)
+
+# D.remove(1)
+# print(D)
+
+# D3=set(D)
+# print(D3)
+
+# 13] print(D.index(2))
+
+# 15] D.clear()
+# print(D)
+
+# D3=D
+# print(D3)
+
+# 16] T1=(1,2,3,4,5,6)
+# print(T1[-1])
+# print(T1[0]) 
+
+# 17] L=list(T1)
+# print(L)
+# T2=tuple(L)
+# print(T2)
+
+# 18] my_tuple = (10,20,30,40,50)
+# if 30 in my_tuple:
+#     print("30 exists in tuple.")
+# else:
+#     print("30 does not exist in tuple.t
+
+# 19] t1=(1,2,3,4,5)
+# t2=(5,6,7,8)
+# add=t1+t2
+# print(add)
